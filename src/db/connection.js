3b5e40b8a1c4e4233735dc81f881afd72c284398
Steve P <56083929+spp54@users.noreply.github.com>
2022-08-23 12:04:19 +0100

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-console.log("what the fuck3")
 const { Sequelize } = require("sequelize");
 
 
-console.log("what the fuck")
+
 exports.sequelize = new Sequelize(process.env.MYSQL_URI);
    
